422815fdde374abfe6ca2c137d8e153807fbc975
Luncek-33 <l.vojnovic09@gmail.com>
2025-11-18 21:36:36 +0100

    l 20, hard chellange

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
 import Swiper from "react-native-swiper";
 
@@ -121,6 +121,101 @@ const styles = StyleSheet.create({
     marginTop:10,
     borderRadius:8,
    }
+});
+
+export default Home;*/
+
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import Swiper from "react-native-swiper";
+
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+      currentIndex: 1,
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetch("https://691ccee63aaeed735c9232a0.mockapi.io/api/images/images");
+    const jsonData = await data.json();
+    this.setState({ images: jsonData });
+  }
+
+  render() {
+    const { images, currentIndex } = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={styles.sliderContainer}>
+          {images.length > 0 && (
+            <>
+              <Swiper
+                autoplay={true}
+                activeDotColor="#22D4FFFF"
+                autoplayTimeout={5}
+                onIndexChanged={(index) => this.setState({ currentIndex: index + 1 })}
+              >
+                {images.map((item, i) => (
+                  <View style={styles.item} key={i}>
+                    <Image style={styles.imgItem} source={{ uri: item.uri }} resizeMode="cover" />
+                  </View>
+                ))}
+              </Swiper>
+              <View style={styles.pagination}>
+                <Text style={styles.paginationText}>
+                  {currentIndex} / {images.length}
+                </Text>
+              </View>
+            </>
+          )}
+        </View>
+
+     
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  sliderContainer: {
+    width: "90%",
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    borderRadius: 8,
+  },
+  item: {
+    flex: 1,
+    justifyContent: "center",
+    borderRadius: 8,
+  },
+  imgItem: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 8,
+  },
+  pagination: {
+    position: "absolute",
+    bottom: 10,
+    alignSelf: "center",
+    backgroundColor: "rgba(0,0,0,0.4)",
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+    borderRadius: 10,
+  },
+  paginationText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+ 
+
 });
 
 export default Home;
